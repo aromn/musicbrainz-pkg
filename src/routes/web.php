d5c8b2c974abf8caa6580806130be66df93b18b8
Aldo Romero <aldoromnn@gmail.com>
2020-11-03 11:36:17 -0600

    Added route to dropdown view

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,4 +5,5 @@ Route::group(['namespace' => 'Aromn\Musicbrainz\Http\Controllers'], function(){
 	Route::get('event/{query}', 'MusicbrainzController@searchEvent');
 	Route::get('label/{query}', 'MusicbrainzController@searchLabel');
 	Route::get('lookup/{entity}/{mbdi}/{inc?}', 'MusicbrainzController@lookup');		
+	Route::get('ddown', 'MusicbrainzController@ddown');
 });
