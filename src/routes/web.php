1df39fb7ad3791b1322fa04709ec04d74adfb73f
Aldo Romero <aldoromnn@gmail.com>
2020-11-03 10:18:42 -0600

    Added route for lookup function

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,5 +4,5 @@ Route::group(['namespace' => 'Aromn\Musicbrainz\Http\Controllers'], function(){
 	Route::get('artist/{artist}/{type?}/{country?}', 'MusicbrainzController@searchArtist');	
 	Route::get('event/{query}', 'MusicbrainzController@searchEvent');
 	Route::get('label/{query}', 'MusicbrainzController@searchLabel');
-	Route::get('testing', 'MusicbrainzController@test');		
+	Route::get('lookup/{entity}/{mbdi}/{inc?}', 'MusicbrainzController@lookup');		
 });
