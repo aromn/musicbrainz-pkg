6cdf295d1d876ae16dca970dd3ba2c3d5cc5da1f
Aldo Romero <aldoromnn@gmail.com>
2020-11-07 11:41:06 -0600

    Service provider: Removed views load

--- src/MusicbrainzServiceProvider.php
+++ src/MusicbrainzServiceProvider.php
@@ -9,7 +9,6 @@ class MusicbrainzServiceProvider extends ServiceProvider
 	public function boot()
 	{
 		$this->loadRoutesFrom(__DIR__.'/routes/web.php');
-		$this->loadViewsFrom(__DIR__.'/views', 'muzicbrains');
 	}
 
 	public function register()
